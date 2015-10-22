edf2d9fa12773ac1b0de850d586874787f035af9
Rdheew <Rdheew>
2015-10-22 19:51:17 +0000

    Add message create View

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+  name = models.CharField(max_length=300)
+  email = models.CharField(max_length=300)
+  message = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def __unicode__(self):
+      return self.name
