f7a7721c7f2acd823e269f26889a432b0167b93a
Rdheew <Rdheew>
2015-10-22 20:27:28 +0000

    Add styling to message form

--- core/views.py
+++ core/views.py
@@ -3,13 +3,20 @@ from django.views.generic import TemplateView
 from django.views.generic import CreateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
+
+
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+
 class MessageCreateView(CreateView):
     model = Message
-    template_name = "message/message_form.html"
+    template_name = "Message/Message_form.html"
     fields = ['name','email', 'message']
     success_url = reverse_lazy('success')
+
+
+
+
 class Success(TemplateView):
   template_name = "success.html"
